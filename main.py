b1aceb07fdac81799f6105bc07144dd9ef884863
Lynoma <85151448+Lynoma@users.noreply.github.com>
2022-09-14 23:09:24 +0200

    Update comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Objects.Meal import Meal
 
 
 def run():
-    #Initialise db
+    # Initialise db
     db = SqlHelper()
 
     meal = Meal('Thon Mayo', 'Pomme', 14)
@@ -12,7 +12,7 @@ def run():
     arr = [meal, Meal('Jambon', 'Banane', 2)]
     db.add_items(arr)
 
-    #close db
+    # close db
     del db
 
 
